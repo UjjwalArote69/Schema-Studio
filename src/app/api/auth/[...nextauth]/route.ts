6d528361eade06071d0346eb90bcc51036929a86
UjjwalArote69 <ujjwalarote69@gmail.com>
2026-03-26 14:57:56 +0530

    idk

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,12 +4,12 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter"; // <-- 1. Import this
-import prisma  from "@/lib/prisma";
+import {prisma}  from "@/lib/prisma";
 import bcrypt from "bcryptjs";
 
 export const authOptions: NextAuthOptions = {
   // 2. Add the adapter here! This is required for OAuth (Google/Github) to save users.
-  adapter: PrismaAdapter(prisma) as any, 
+  adapter: PrismaAdapter(prisma as any), 
   
   providers: [
     GithubProvider({
--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,5 +1,5 @@
 
-import prisma  from "@/lib/prisma";
+import {prisma}  from "@/lib/prisma";
 import bcrypt from "bcryptjs";
 import { NextResponse } from "next/server";
 
