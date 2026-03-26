dd756fb7a0cbe4ce2d95f4f0a99effff9201ca83
UjjwalArote69 <ujjwalarote69@gmail.com>
2026-03-26 15:00:13 +0530

    idk

--- src/app/actions/user-actions.ts
+++ src/app/actions/user-actions.ts
@@ -2,7 +2,7 @@
 
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
-import prisma  from "@/lib/prisma";
+import {prisma}  from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
